3db5849c94143777f192a1a9d5595b2926567e8c
Anthony Nugussie <nugussie0745@gmail.com>
2025-08-25 11:48:50 +0300

    avatar fix

--- client/components/Testimonials.jsx
+++ client/components/Testimonials.jsx
@@ -5,7 +5,7 @@ const Testimonials = () => {
     <div className="text-center mb-12">
       <h1 className="text-secondary text-2xl">Testimonials</h1>
       <div className="w-full p-6 flex gap-2 justify-around items-center">
-        <div className="h-[50px] min-w-[50px] w-[50px] rounded-[50%] bg-neutral-500 mr-6 self-center"></div>
+        <div className="h-[100px] min-w-[100px] w-[100px] rounded-[50%] bg-neutral-500 mr-6 self-center"></div>
         <p className="text-neutral-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
